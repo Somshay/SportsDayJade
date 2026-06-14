60fb79ce706eb27dc8e6bc176c29568f372a98b2
Somshay <tmedinelli@gmail.com>
2026-06-14 21:06:32 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://twxcionagmkvgtvgishf.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3eGNpb25hZ21rdmd0dmdpc2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNTYzMDUsImV4cCI6MjA5NjkzMjMwNX0.LDVDLrEFILYDtRcD6_-ks0kzlmKlNSErPpKAz7wAkRI"
+};
